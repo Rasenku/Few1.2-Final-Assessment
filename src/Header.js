f896e80d034d22e8be6d2dc64b69cbfb5991c7f8
Dahveyea <dahveyea.cowan@gmail.com>
2021-12-09 20:45:04 -0800

    Challenge 2

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header(){
+    return (
+        <div>
+            <h1>FEW 1.2 Final Assessment</h1>
+            <h2>Dahveyea Cowan</h2>
+        </div>
+
+    );
+
+}
+
+export default Header;
